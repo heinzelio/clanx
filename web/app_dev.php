afa780bca498248b89288d794f1088aa6b6475df
chriglburri <chriglburri@gmail.com>
2016-04-08 19:42:04 +0200

    reverted that hack I used to need when I was developing and resting remotely

--- web/app_dev.php
+++ web/app_dev.php
@@ -11,10 +11,6 @@ use Symfony\Component\Debug\Debug;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 
-
-// Changes by Chrigl:
-// Temporarily disable the locking of app_dev during development.
-/*
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1']) || php_sapi_name() === 'cli-server')
@@ -22,8 +18,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
-*/
-// End of changes by Chrigl.
 
 /**
  * @var Composer\Autoload\ClassLoader $loader
