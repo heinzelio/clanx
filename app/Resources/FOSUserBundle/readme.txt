6600be2880988de983db854574c061ed81ba273a
chriglburri <chriglburri@gmail.com>
2016-04-27 17:31:59 +0200

    We have now a customized user profile form. The users may now enter personal information.

--- app/Resources/FOSUserBundle/readme.txt
+++ app/Resources/FOSUserBundle/readme.txt
@@ -3,3 +3,7 @@ This folder "app/Resources/FOSUserBundle/views" is a copy of the folder
 
 It is used to overwrite the default layout.
 We can do here whatever we want.
+
+
+Also check out this tutorial video:
+https://knpuniversity.com/screencast/fosuserbundle-ftw/fos-user-bundle
---
+++ src/AppBundle/Form/ProfileFormType.php
@@ -0,0 +1,96 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AppBundle\Form;
+
+use FOS\UserBundle\Util\LegacyFormHelper;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+
+class ProfileFormType extends AbstractType
+{
+    private $class;
+
+    /**
+     * @param string $class The User class name
+     */
+    public function __construct($class)
+    {
+        $this->class = $class;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $this->buildUserForm($builder, $options);
+
+        // $builder->add('current_password', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'), array(
+        //     'label' => 'form.current_password',
+        //     'translation_domain' => 'FOSUserBundle',
+        //     'mapped' => false,
+        //     'constraints' => new UserPassword(),
+        // ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => $this->class,
+            'csrf_token_id' => 'profile',
+            // BC for SF < 2.8
+            'intention'  => 'profile',
+        ));
+    }
+
+    // BC for SF < 2.7
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $this->configureOptions($resolver);
+    }
+
+    // BC for SF < 3.0
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'fos_user_profile';
+    }
+
+    /**
+     * Builds the embedded form representing the user.
+     *
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    protected function buildUserForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('forename')
+            ->add('surname')
+            ->add('gender', GenderType::class)
+            ->add('dateOfBirth', DateType::class, array('widget' => 'single_text')) // this makes a html5 date picker. Awesom!
+            ->add('street')
+            ->add('zip')
+            ->add('city')
+            ->add('country')
+            ->add('phone')
+            ->add('occupation')
+            ->add('username', null, array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'))
+            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
+        ;
+    }
+}
