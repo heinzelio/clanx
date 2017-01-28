f0d6728804739ea20df137cdda598de239933a6e
chriglburri <chriglburri@gmail.com>
2017-01-28 17:27:40 +0100

    For #7 added question and answer domain models.

---
+++ src/AppBundle/Entity/Answer.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Answer
+ *
+ * @ORM\Table(name="answer", indexes={@ORM\Index(name="question_key", columns={"question_id"}), @ORM\Index(name="commitment_key", columns={"commitment_id"})})
+ * @ORM\Entity
+ */
+class Answer
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="answer", type="string", length=1000, nullable=true)
+     */
+    private $answer;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var \AppBundle\Entity\Question
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Question")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="question_id", referencedColumnName="id")
+     * })
+     */
+    private $question;
+
+    /**
+     * @var \AppBundle\Entity\Commitment
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Commitment")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="commitment_id", referencedColumnName="id")
+     * })
+     */
+    private $commitment;
+
+
+
+    /**
+     * Set answer
+     *
+     * @param string $answer
+     *
+     * @return Answer
+     */
+    public function setAnswer($answer)
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    /**
+     * Get answer
+     *
+     * @return string
+     */
+    public function getAnswer()
+    {
+        return $this->answer;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set question
+     *
+     * @param \AppBundle\Entity\Question $question
+     *
+     * @return Answer
+     */
+    public function setQuestion(\AppBundle\Entity\Question $question = null)
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    /**
+     * Get question
+     *
+     * @return \AppBundle\Entity\Question
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+
+    /**
+     * Set commitment
+     *
+     * @param \AppBundle\Entity\Commitment $commitment
+     *
+     * @return Answer
+     */
+    public function setCommitment(\AppBundle\Entity\Commitment $commitment = null)
+    {
+        $this->commitment = $commitment;
+
+        return $this;
+    }
+
+    /**
+     * Get commitment
+     *
+     * @return \AppBundle\Entity\Commitment
+     */
+    public function getCommitment()
+    {
+        return $this->commitment;
+    }
+}
