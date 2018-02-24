6d2e4a55113dd6440c77d886b026fa00ce0f4923
Chrigl <chriglburri@gmail.com>
2018-02-25 00:07:20 +0100

    update deploy script

--- deploy/dev_deploy.ps1
+++ deploy/dev_deploy.ps1
@@ -32,15 +32,28 @@ Write-Host "warm up the cache..."
 php .\bin\console cache:warmup --env=prod
 
 [console]::ForegroundColor = "Green"
-Write-Host "check requirements..."
-php ./bin/symfony_requirements
-
-[console]::ForegroundColor = "Blue"
 Write-Host "remove dev files..."
 Remove-Item -Force -Recurse .\.git
 Remove-Item -Force -Recurse .\assets
 Remove-Item -Force -Recurse .\node_modules
 Remove-Item -Force -Recurse .\sql
+Remove-Item -Force .env.dist
+Remove-Item -Force .gitignore
+#Keep composer.json. The Kernel.php needs it!
+#Remove-Item -Force composer.json
+Remove-Item -Force composer.lock
+Remove-Item -Force mklink.bat
+Remove-Item -Force package-lock.json
+Remove-Item -Force package.json
+Remove-Item -Force phpunit.xml.dist
+Remove-Item -Force README.md
+Remove-Item -Force symfony.lock
+Remove-Item -Force webpack.config.js
+Remove-Item -Force yarn-error.log
+Remove-Item -Force yarn.lock
+
+#Check the page if it runs locally
+Start-Process -FilePath "http://localhost/clanx_test/info/"
 
 # go back to where we came from
 [console]::ForegroundColor = $originColor
