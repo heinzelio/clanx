ca59cbdc730e73a647ba41ce67b85cc6c1c4d92a
Chrigl <chriglburri@gmail.com>
2018-02-25 13:34:17 +0100

    fixed path

--- mkLink.bat
+++ mkLink.bat
@@ -2,6 +2,6 @@ cd ..
 mklink /D htdocs C:\Bitnami\wamp\apache2\htdocs
 cd C:\Bitnami\wamp\apache2\htdocs\
 mklink /D clanx_deploy c:\Users\chrig\Documents\dev\clanx.deploy\public
-mklink /D clanx_dev c:\Users\chrig\Documents\dev\clanx.deploy\public
+mklink /D clanx_dev c:\Users\chrig\Documents\dev\clanx.git\public
 
 PAUSE
