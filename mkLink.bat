40b422432d1534b5558d5143514be0228de45ca0
Chrigl <chriglburri@gmail.com>
2018-02-25 11:05:36 +0100

    make more links

--- mkLink.bat
+++ mkLink.bat
@@ -1,4 +1,7 @@
+cd ..
+mklink /D htdocs C:\Bitnami\wamp\apache2\htdocs
 cd C:\Bitnami\wamp\apache2\htdocs\
-mklink /D clanx4 c:\Users\chrig\Documents\dev\clanx.git\public
+mklink /D clanx_deploy c:\Users\chrig\Documents\dev\clanx.deploy\public
+mklink /D clanx_dev c:\Users\chrig\Documents\dev\clanx.deploy\public
 
 PAUSE
